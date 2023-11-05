eb20d59f8f85bf536cf97cfda979170db651eb73
Marcelo Melogno <marcelomelogno1986@gmail.com>
2023-11-04 23:05:58 -0300

    scrollreveal.js

--- js/main.js
+++ js/main.js
@@ -24,3 +24,48 @@ window.addEventListener("scroll", () => {
     logo.style.height = '110px';
   }
 });
+
+window.sr = ScrollReveal();
+
+sr.reveal('.toright', {
+    duration: 5000,
+    origin: 'right',
+    distance: '-250px'
+});
+sr.reveal('.toleft', {
+    duration: 4500,
+    origin: 'left',
+    distance: '-250px'
+});
+sr.reveal('.todown', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-200px'
+});
+sr.reveal('.toup', {
+    duration: 4000,
+    origin: 'top',
+    distance: '-250px'
+});
+sr.reveal('.todown2', {
+    duration: 2500,
+    origin: "bottom",
+    distance: '-50px',
+    
+}) 
+
+sr.reveal('.revealFast',{
+  duration: 1500
+})
+
+sr.reveal('.reveal',{
+  duration: 3000
+})
+
+sr.reveal('.revealSlow',{
+  duration: 4500
+})
+
+sr.reveal('.revealExtraSlow',{
+  duration: 6000
+})
