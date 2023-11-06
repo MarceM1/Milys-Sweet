a41e644ea8fdbb3159c0fe396b8ca1e3589f9aab
Marcelo Melogno <marcelomelogno1986@gmail.com>
2023-11-06 00:57:33 -0300

    Cupcake section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+//Cambio de color en navbar
 const navbr = document.querySelector("#nav");
 const links = document.querySelectorAll(".nav-link, #scrollTextColor");
-const svg = document.querySelector('#Capa_1')
-const logo = document.querySelector('.logo')
+const svg = document.querySelector("#Capa_1");
+const logo = document.querySelector(".logo");
 
 window.addEventListener("scroll", () => {
   const scrollYBg = window.scrollY;
@@ -12,60 +13,60 @@ window.addEventListener("scroll", () => {
       link.classList.add("text-white");
     });
     svg.setAttribute("fill", "white");
-    logo.style.width = '87px';
-    logo.style.height = '60px';
+    logo.style.width = "87px";
+    logo.style.height = "60px";
   } else {
     navbr.classList.remove("scrolled_bg");
     links.forEach((link) => {
       link.classList.remove("text-white");
     });
     svg.setAttribute("fill", "#555");
-    logo.style.width = '150px';
-    logo.style.height = '110px';
+    logo.style.width = "150px";
+    logo.style.height = "110px";
   }
 });
 
+//Animaciones de ScrollReveal
 window.sr = ScrollReveal();
 
-sr.reveal('.toright', {
-    duration: 5000,
-    origin: 'right',
-    distance: '-250px'
+sr.reveal(".toright", {
+  duration: 5000,
+  origin: "right",
+  distance: "-250px",
 });
-sr.reveal('.toleft', {
-    duration: 4500,
-    origin: 'left',
-    distance: '-250px'
+sr.reveal(".toleft", {
+  duration: 4500,
+  origin: "left",
+  distance: "-250px",
 });
-sr.reveal('.todown', {
-    duration: 3000,
-    origin: 'bottom',
-    distance: '-200px'
+sr.reveal(".todown", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "-200px",
 });
-sr.reveal('.toup', {
-    duration: 4000,
-    origin: 'top',
-    distance: '-250px'
+sr.reveal(".toup", {
+  duration: 4000,
+  origin: "top",
+  distance: "-250px",
 });
-sr.reveal('.todown2', {
-    duration: 2500,
-    origin: "bottom",
-    distance: '-50px',
-    
-}) 
+sr.reveal(".todown2", {
+  duration: 2500,
+  origin: "bottom",
+  distance: "-250px",
+});
 
-sr.reveal('.revealFast',{
-  duration: 1500
-})
+sr.reveal(".revealFast", {
+  duration: 1500,
+});
 
-sr.reveal('.reveal',{
-  duration: 3000
-})
+sr.reveal(".reveal", {
+  duration: 3000,
+});
 
-sr.reveal('.revealSlow',{
-  duration: 4500
-})
+sr.reveal(".revealSlow", {
+  duration: 4500,
+});
 
-sr.reveal('.revealExtraSlow',{
-  duration: 6000
-})
+sr.reveal(".revealExtraSlow", {
+  duration: 6000,
+});
